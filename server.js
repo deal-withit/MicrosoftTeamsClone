3c934732bc78ce9aaee2604f568fb13d02192bcf
Kundan Kumar <kundan2599sinha@gmail.com>
2021-07-07 14:57:43 +0530

    port

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
-server.listen(3000, () => {
-    console.log("Listening on port 3000...")
+var port = process.env.PORT || 3000;  
+server.listen(port, () => {
+    console.log("Listening on port...")
 })
 
 const webSocket = new Socket({ httpServer: server })
